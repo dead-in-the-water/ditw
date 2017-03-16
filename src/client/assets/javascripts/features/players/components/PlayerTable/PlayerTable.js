6f73bfbf931454598719269d25a5d05094b07ae6
Marc Leavitt <mgleavitt@Marcs-MacBook-Pro.local>
2017-03-15 22:00:26 -0700

    Another micro-commit, but this time the Table works (thanks to Daniel for suggesting work-around). Turns out that the elements of a table DO NOT want to be separated by intervening elements (doing something directly w/children?)

--- src/client/assets/javascripts/features/players/components/PlayerTable/PlayerTable.js
+++ src/client/assets/javascripts/features/players/components/PlayerTable/PlayerTable.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 
-import PlayerTableItem from '../PlayerTableItem'
+//import PlayerTableItem from '../PlayerTableItem'
 import './PlayerTable.scss'
 
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table'
@@ -14,76 +14,19 @@ export default class PlayerTable extends Component {
   renderList() {
 		return this.props.players.map((Player, i) =>
 		  (
-				<PlayerTableItem
-				  key={i}
-				  id={Player.id}
-				  name={Player.firstName + ' ' + Player.lastName}
-				  gamesPlayed={Player.gamesPlayed}
-				  inThisGame={Player.inThisGame}
-				  {...this.props.actions} 
-				/>
+	    <TableRow className='player-table-row'>
+        <TableRowColumn>{Player.id}</TableRowColumn>
+        <TableRowColumn>{Player.firstName + ' ' + Player.lastName}</TableRowColumn>
+        <TableRowColumn>{Player.gamesPlayed}</TableRowColumn>
+      </TableRow>
 		  )
 		)
   }
 
-renderList1() {
-	return (
-		  <TableRow>
-		    <TableRowColumn>1</TableRowColumn>
-		    <TableRowColumn>John Adams</TableRowColumn>
-		    <TableRowColumn>3</TableRowColumn>
-		  </TableRow>
-	)
-}
-
-renderList2() {
-	return (
-		  <TableRow>
-		    <TableRowColumn>2</TableRowColumn>
-		    <TableRowColumn>Thomas Jefferson</TableRowColumn>
-		    <TableRowColumn>300</TableRowColumn>
-		  </TableRow>
-	)
-}
-
-renderList3() {
-	return (this.renderList1() + this.renderList2())
-}
-
-renderList4() {
-	return (
-			<div>
-			  <TableRow>
-			    <TableRowColumn>1</TableRowColumn>
-			    <TableRowColumn>John Adams</TableRowColumn>
-			    <TableRowColumn>3</TableRowColumn>
-			  </TableRow>
-			  <TableRow>
-			    <TableRowColumn>2</TableRowColumn>
-			    <TableRowColumn>Thomas Jefferson</TableRowColumn>
-			    <TableRowColumn>300</TableRowColumn>
-			  </TableRow>
-		  </div>
-	)
-}
-
-renderList5() {
-	return (
-				<PlayerTableItem
-				  key='0'
-				  id='1'
-				  name='Benjamin Franklin'
-				  gamesPlayed='122'
-				  inThisGame='false'
-				  {...this.props.actions} 
-				/>
-	)
-}
-
   render() {
 		return (
-		  <Table className='player-table'>
-				<TableHeader className='player-table-header'>
+		  <Table className='player-table' multiSelectable={true}>
+				<TableHeader className='player-table-header' displaySelectAll={false}>
 				  <TableRow className='player-table-header-row'>
 						<TableHeaderColumn>Player Number</TableHeaderColumn>
 						<TableHeaderColumn>Name</TableHeaderColumn>
@@ -91,7 +34,7 @@ renderList5() {
 				  </TableRow>
 				</TableHeader>
 			<TableBody className='player-table-body'>
-			  {this.renderList4()}
+			  {this.renderList()}
 			</TableBody>
 		  </Table>
 		)
